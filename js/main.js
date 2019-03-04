4c4342deafa73dd7e81c1e60939a4b5e6435c27f
Collier <v_collier@tss-hr420-dm18.fc.ca>
2019-03-04 12:25:32 -0500

    adding JS

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(() => {
+	// hook up the Js file
+	console.log("fired! ready to jam out");
+
+	function playDrumSound(e) {
+		// e in round brackets is the event object ->
+		// gets generated with every event and passes some data through
+		//debugger;
+		//
+		//try selecting the matching element
+		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+		// the return keyword stops javascript from running (any code below it)
+		if (!audio) { return; }
+		
+
+		audio.currentTime = 0;
+		audio.play();
+	}
+
+	window.addEventListener("keydown", playDrumSound);
+})();
