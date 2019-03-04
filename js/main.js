c1be7ac814f17b827b3b4abcc65d40faf9dfa211
Collier <v_collier@tss-hr420-dm18.fc.ca>
2019-03-04 13:19:11 -0500

    adding UI

--- js/main.js
+++ js/main.js
@@ -16,7 +16,29 @@
 
 		audio.currentTime = 0;
 		audio.play();
+
+		// select the parent div and animate it
+		// try selecting the matching parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing');
+		
+	}
+
+	function removePlayingClass(e) {
+		// listen for a CSS transition to finish and then reset the element
+		// by removing the playing class
+
+	if (e.propertyName !== "transform") {
+			return;
+		} else {
+			console.log('do some stuff here, transition is done');
+			e.currentTarget.classList.remove('playing');
+		}
 	}
 
 	window.addEventListener("keydown", playDrumSound);
+
+	const keys = Array.from(document.querySelectorAll('.key'));
+
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
 })();
